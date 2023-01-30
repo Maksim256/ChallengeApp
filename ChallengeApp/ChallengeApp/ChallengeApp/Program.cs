9b3c6cb914873b054a2a0206d29b9b47b41a4ae0
Andrzej <andrzej.o@windowslive.com>
2023-01-30 19:05:23 +0100

    typy zmiennych

--- ChallengeApp/ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World1!");
+﻿var myAge = 33;
+int myAge2 = 120;
+int newAge = myAge2 +5;
+Console.WriteLine(newAge);
+// liczby całkowite
+int myVar = int.MinValue;
+uint myVar2 = uint.MaxValue;
+long myVar3 = long.MaxValue;
+//liczby zmiennoprzecoinowe
+float myNumber = float.MaxValue;
+double myNumber2 = double.MaxValue;
+decimal myNumber3 = decimal.MaxValue;
+
+//zmienn string
+string name = "Andrzej";
+string surename = "Maks";
+string result = name + surename  + myAge;
+Console.WriteLine(result);
