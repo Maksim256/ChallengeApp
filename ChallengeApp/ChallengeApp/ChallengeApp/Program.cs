9a800d2c7bbfd70d66dfe01d7eb6939f303ecffe
Andrzej <andrzej.o@windowslive.com>
2023-01-31 21:37:36 +0100

    Lekcja po logice

--- ChallengeApp/ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/ChallengeApp/Program.cs
@@ -1,6 +1,9 @@
-﻿var myAge = 33;
-int myAge2 = 120;
-int newAge = myAge2 +5;
+﻿//var myAge = 33;
+using System.Linq;
+
+int myAge = 120;
+int newAge = myAge
+    +5;
 Console.WriteLine(newAge);
 // liczby całkowite
 int myVar = int.MinValue;
@@ -16,3 +19,12 @@ string name = "Andrzej";
 string surename = "Maks";
 string result = name + surename  + myAge;
 Console.WriteLine(result);
+char myVar5 = 'c';
+var result2 = name.ToArray();
+
+//zmienna logiczna
+bool isActive = true;
+isActive = false;
+var isValid = 5 > 6;
+Console.WriteLine(isValid);
+
