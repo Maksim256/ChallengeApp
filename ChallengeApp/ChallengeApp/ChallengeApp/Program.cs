9bc1d0e905a831031851f0cf230fa244aa7c67a1
Andrzej <andrzej.o@windowslive.com>
2023-02-06 19:12:29 +0100

    Zadanie domowe dzień 3

--- ChallengeApp/ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/ChallengeApp/Program.cs
@@ -1,63 +1,44 @@
-﻿//var myAge = 33;
-using System.Linq;
+﻿using System.Xml.Linq;
 
-int myAge = 120;
-int newAge = myAge
-    + 5;
-Console.WriteLine(newAge);
-// liczby całkowite
-int myVar = int.MinValue;
-uint myVar2 = uint.MaxValue;
-long myVar3 = long.MaxValue;
-//liczby zmiennoprzecoinowe
-float myNumber = float.MaxValue;
-double myNumber2 = double.MaxValue;
-decimal myNumber3 = decimal.MaxValue;
+string imie = "Andrzej";
+bool mezczyzna = false;
+int age = 29;
 
-//zmienn string
-string name1 = "Andrzej";
-string surename = "Maks";
-string result = name1 + surename + myAge;
-Console.WriteLine(result);
-char myVar5 = 'c';
-var result2 = name1.ToArray();
-
-//zmienna logiczna
-bool isActive = true;
-isActive = false;
-var isValid = 5 > 6;
-Console.WriteLine(isValid);
-
-var number1 = 15;
-var number2 = 10;
-
-var name = "Adam";
-var age = 53;
-//&&  log i
-// ||  log lub
-// ! negacja
-// np:  if(!(age < 50))
-if (age > 50)
+Console.WriteLine("zmienna imie: " + imie);
+Console.WriteLine("zmienn plec, czy meżczyzna: " + mezczyzna);
+Console.WriteLine("zmienna wiek: " + age);
+Console.WriteLine("     ");
+if (mezczyzna == true)
 {
-    if (name == "Adama")
+    if (age < 18)
     {
-        Console.WriteLine("Jestem Adam");
+        Console.WriteLine("Niepełnoletni Mężczyzna");
     }
-    else
+    if (age >= 18)
     {
-        Console.WriteLine("Mam inne imie");
+        Console.WriteLine("Pełnoletni mezczyzna");
     }
-    // Console.WriteLine("Jestem Adamem i mam mniej niż 50lat");
+
 }
-else if (age > 40)
-{
-    Console.WriteLine("40-50 latek");
-}
-else if (age > 30)
-{
-    Console.WriteLine("30 latek");
-}
+
+
+
 else
 {
-    Console.WriteLine("inny wiek");
+    if (age == 33 )
+    {
+        Console.WriteLine("Ewa, lat 33 ");
+    }
+    else if (age < 30 )
+    {
+        Console.WriteLine("Kobieta poniżej 30 lat");
+    }
+    else if (age >= 30)
+    {
+        Console.WriteLine("Kobieta");
+    }
+
 }
+
+
+
