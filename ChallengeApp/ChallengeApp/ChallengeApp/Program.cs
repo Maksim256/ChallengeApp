d4f293d11f599d5f3518602b8655b50742a8e4cf
Andrzej <andrzej.o@windowslive.com>
2023-02-07 13:32:01 +0100

    Tablice i Listy

--- ChallengeApp/ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/ChallengeApp/Program.cs
@@ -1,1 +1,20 @@
-﻿
+﻿int[] grades = new int[51];
+//string[] dayOfWeeks = new string[7];
+
+List<string> dayOfWeeks = new List<string>();
+dayOfWeeks.Add("poniedziałek");
+dayOfWeeks.Add("wtorek");
+dayOfWeeks.Add("środa");
+dayOfWeeks.Add("czwaretek");
+dayOfWeeks.Add("piątek");
+dayOfWeeks.Add("sobota");
+dayOfWeeks.Add("niedziela");
+
+//for(int i=0;i<dayOfWeeks.Count;i++)
+//{
+//    Console.WriteLine(dayOfWeeks[i]);
+//}
+foreach(var day in dayOfWeeks)
+{
+    Console.WriteLine(day);
+}
