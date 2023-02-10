a76d10765ca09ec5e32ad01a49013a839d14c1b1
Andrzej <andrzej.o@windowslive.com>
2023-02-10 18:53:40 +0100

    Zadanie domowe dzień 6- oczyszczony kod

--- ChallengeApp/ChallengeApp/ChallengeApp/Employee.cs
+++ ChallengeApp/ChallengeApp/ChallengeApp/Employee.cs
@@ -1,40 +1,24 @@
-﻿using System.Collections.Generic;
-using System;
-using System.Runtime.CompilerServices;
-using System.Security.Cryptography.X509Certificates;
-using ChallengeApp;
-
-namespace ChallengeApp
+﻿namespace ChallengeApp
 {
     public class Employee
     {
-        // public string imie;
-        //public string nazwisko;
-        //public int wiek;
-        //public int punkty;
         private List<int> scoreA = new List<int>();
-        // List<Employee> employees = new List<Employee>()
-        //{
-            
-        //};
 
         public Employee(string imie, string nazwisko, int wiek)
         {
             this.Imie = imie;
             this.Nazwisko = nazwisko;
             this.Wiek = wiek;
-            //this.Punkty = punkty;
         }
 
         public string Imie { get; private set; }
         public string Nazwisko { get; private set; }
         public int Wiek { get; private set; }
-       public int Wynik
-        { 
+        public int Wynik
+        {
             get
             {
                 return this.scoreA.Sum();
-                //return this.employees.Sum();
             }
         }
         public int Maksimum
@@ -43,17 +27,7 @@ namespace ChallengeApp
             {
                 return this.scoreA.Max();
             }
-
         }
-        //public int SortA
-        //{
-        //    get 
-        //    {
-        //        return this.scoreA.Sort();
-        //    }
-        //}
-
-        
 
         public void AddPunty(int numberA)
         {
--- ChallengeApp/ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/ChallengeApp/Program.cs
@@ -1,23 +1,8 @@
 ﻿using ChallengeApp;
-using System.Collections.Generic;
-using System.Runtime.InteropServices.Marshalling;
-
-User user1 = new User("Adam", "32432fddf");
-User user2 = new User("Monika", "32432fddf");
-User user3 = new User("Zuzia", "32432fddf");
-User user4 = new User("Damian", "32432fddf");
-
-
-//user1.password = "password";
 
 Employee employee1 = new Employee("Adam", "Adamowski", 30);
 Employee employee2 = new Employee("Ewa", "Ewowska", 18);
 Employee employee3 = new Employee("Kamil", "Kamilski", 48);
-
-//user1.AddScore(4);
-
-
-//Console.WriteLine(employee1.PunktyPrac);
 
 employee1.AddPunty(1);
 employee1.AddPunty(2);
@@ -57,16 +42,9 @@ foreach(var employee in employees)
 }
 Console.WriteLine("rekord z listy to:" + employeeWithMaxResult.Nazwisko+" Punktów: "+ employeeWithMaxResult.Wynik);
 
-//ten Login dostaje dostep do propercji o tej samej nazwie
-//user1.Login = "Marek";
-var name = user1.Login;
-user1.AddScore(5);
-user1.AddScore(2);
-var result = user1.Result;
 var wynik1 = employee1.Wynik;
 var wynik2 = employee2.Wynik;
 var wynik3 = employee3.Wynik;
-//Console.WriteLine(wynik);
 
 if(employee1.Wynik > employee2.Wynik && employee1.Wynik > employee3.Wynik)
 {
@@ -83,10 +61,6 @@ else
     int rekord = employee3.Wynik;
     Console.WriteLine("najwięcej punktów " + rekord+" "+ "Zdobył: "+ employee3.Nazwisko);
 }
-//do zmiennej statycznej odwołujemy sie przez obiekt i nie musimy jej tworzyć
-var name1 = User.GameName;
-
-
 Console.WriteLine(employee1.Imie + " " + employee1.Nazwisko + " Lat " + employee1.Wiek + " wynik "+ wynik1);
 Console.WriteLine(employee2.Imie + " " + employee2.Nazwisko + " Lat " + employee2.Wiek + " wynik " + wynik2);
 Console.WriteLine(employee3.Imie + " " + employee3.Nazwisko + " Lat " + employee3.Wiek + " wynik " + wynik3);
